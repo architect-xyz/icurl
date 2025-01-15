5f27d7d102af79c5d4c8b75b01e9adbe0489da42
Andrew Lee <126016409+andrew-lee-architect@users.noreply.github.com>
2025-01-15 16:06:09 -0600

    correctly parse Length-Prefixed-Message in responses; add mTLS client certificate auth option

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use anyhow::Result;
-use bytes::{BufMut, Bytes, BytesMut};
+use anyhow::{bail, Context, Result};
+use bytes::{Buf, BufMut, Bytes, BytesMut};
 use clap::Parser;
 use colored::Colorize;
+use std::path::PathBuf;
 use url::Url;
 
 #[derive(Parser)]
@@ -14,13 +15,40 @@ struct Cli {
     /// Provide the request body in $EDITOR
     #[arg(long)]
     editor: bool,
+    /// TLS client certificate auth; specify the certificate file
+    #[arg(long)]
+    cert: Option<PathBuf>,
+    /// TLS client certificate auth; specify the key file;
+    /// if `--cert` is provided but `--key` is not, the keyfile
+    /// location will be guessed from the certfile location.
+    #[arg(long)]
+    key: Option<PathBuf>,
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
     env_logger::init();
     let args = Cli::parse();
-    let client = reqwest::Client::builder().http2_prior_knowledge().build()?;
+    let mut client_builder = reqwest::Client::builder().http2_prior_knowledge();
+
+    // TLS client certificate auth
+    if let Some(cert) = args.cert {
+        let cert_pem = std::fs::read(&cert)
+            .with_context(|| format!("failed to read cert file: {}", cert.display()))?;
+        let key_pem = if let Some(key) = args.key {
+            std::fs::read(&key)
+                .with_context(|| format!("failed to read key file: {}", key.display()))?
+        } else {
+            let mut key_path = cert.clone();
+            key_path.set_extension("key");
+            std::fs::read(&key_path)
+                .with_context(|| format!("failed to read key file: {}", key_path.display()))?
+        };
+        client_builder =
+            client_builder.identity(reqwest::Identity::from_pkcs8_pem(&cert_pem, &key_pem)?);
+    }
+
+    let client = client_builder.build()?;
     let body = if let Some(data) = args.data {
         Some(data)
     } else if args.editor {
@@ -90,8 +118,19 @@ async fn main() -> Result<()> {
                 eprintln!("{}", format!("GRPC {} {}", code, code_reason).red().bold());
             }
         }
-        let res_body = res.text().await?;
-        println!("{}", res_body);
+        let mut buf = res.bytes().await?;
+        if buf.len() < 5 {
+            bail!("invalid Length-Prefixed-Message, expected at least 5 bytes");
+        }
+        let _compressed = buf.get_u8();
+        let len = buf.get_u32();
+        if len == 0 {
+            eprintln!("WARNING: empty response");
+            return Ok(());
+        }
+        let body = buf.get(..).unwrap();
+        let text = std::str::from_utf8(body)?;
+        println!("{}", text);
     }
     Ok(())
 }
